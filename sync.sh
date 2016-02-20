795fd08ce1345ac7dafea8a256f715833488cce0
Chion Tang <sdspeedonion@gmail.com>
2016-02-21 01:02:28 +0800

    Fix newline

--- sync.sh
+++ sync.sh
@@ -23,7 +23,7 @@ if [ $(cat lasterror.log | wc -l) -gt 0 ];then
 fi
 
 if [ $website_test_passed -eq 1 ]; then
-	echo "${website_url}\n" | python magnet_crawler.py > lastsync.log 2> lasterror.log
+	echo -e "${website_url}\n" | python magnet_crawler.py > lastsync.log 2> lasterror.log
 	if [ $(cat lasterror.log | wc -l) -gt 0 ]; then
 		sync_success=0
 	else
@@ -37,9 +37,9 @@ if [ $sync_success -eq 1 ]; then
 	added_magnets=$(expr $(cat magnet_output | grep "magnet:?" | wc -l) - $(cat last_magnet_numbers.txt))
 	echo $(date) > last_sync_success.txt
 	echo $(cat magnet_output | grep "magnet:?" | wc -l) > last_magnet_numbers.txt
-	echo "[$(date)] 同步成功 新增记录${added_magnets}条  \n$(cat synclog.txt)" > synclog.txt
+	echo -e "[$(date)] 同步成功 新增记录${added_magnets}条  \n$(cat synclog.txt)" > synclog.txt
 else
-	echo "[$(date)] 同步失败  \n$(cat synclog.txt)" > synclog.txt
+	echo -e "[$(date)] 同步失败  \n$(cat synclog.txt)" > synclog.txt
 fi
 
 cp readme_header.md README.md
@@ -48,7 +48,7 @@ echo '最后同步成功时间:  '$(cat last_sync_success.txt)'  ' >> README.md
 echo '已抓取磁力链: '$(cat magnet_output | grep "magnet:?" | wc -l)'  ' >> README.md
 echo '本repo已存活: '$(expr $(expr $(date +"%s") - $first_run_time) / 60 / 60 / 24)'天  ' >> README.md
 
-echo '\nLog' >> README.md
+echo -e '\nLog' >> README.md
 echo '----' >> README.md
 cat synclog.txt >> README.md
 
