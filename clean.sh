d84425b2294225af53e7b715733ea019bfdffb95
Chion Tang <sdspeedonion@gmail.com>
2016-02-21 00:36:45 +0800

    Sync on Sun Feb 21 00:36:45 CST 2016

---
+++ clean.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+rm magnet_output
+rm resource_list.json
+rm -rf archives log
+rm synclog.txt
+echo 0 > last_magnet_numbers.txt
+echo '' > last_sync_success.txt
+echo '' > lastsync.log
+echo '' > lasterror.log
